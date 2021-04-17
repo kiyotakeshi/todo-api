76928803ec5a96a4e8cc20a99de34bd2fdc16c94
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-04-18 06:29:11 +0900

    delete unused file

--- src/main/resources/static/js/todo-post.js
+++
@@ -1,7 +0,0 @@
-const apiEndpoint = location.origin + '/api/todo/';
-const todoApi = new TodoApi();
-
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    todoApi.postTodo(apiEndpoint);
-});
